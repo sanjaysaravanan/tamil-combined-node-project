3081cdb931c0050418f2a0fe226310d65e92d042
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2024-05-26 15:37:18 +0530

    added credentials

--- db-utils/mongo-connection.js
+++ db-utils/mongo-connection.js
@@ -1,13 +1,18 @@
 import { MongoClient } from "mongodb";
 
 // Mongo Running URI
-const dbCluster = "127.0.0.1:27017";
-
+// const dbCluster = "127.0.0.1:27017";
+const dbCluster = "cluster0.ruapl1v.mongodb.net";
 const dbName = "tamil-combined";
+const dbUserName = "sanjaysaravanan1997";
+const dbPassword = "8JMtgqJdaf7R0WLm";
 
 const localUri = `mongodb://${dbCluster}/${dbName}`;
 
-const client = new MongoClient(localUri);
+const cloudUri = `mongodb+srv://${dbUserName}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+
+// const client = new MongoClient(localUri);
+const client = new MongoClient(cloudUri);
 
 const db = client.db(dbName);
 
--- db-utils/mongoose-connection.js
+++ db-utils/mongoose-connection.js
@@ -1,13 +1,21 @@
 import mongoose from "mongoose";
 
+/* mongodb+srv://sanjaysaravanan1997:8JMtgqJdaf7R0WLm@cluster0.ruapl1v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 */
+
 const dbName = "tamil-combined";
-const dbCluster = "localhost:27017";
+// const dbCluster = "127.0.0.1:27017";
+const dbCluster = "cluster0.ruapl1v.mongodb.net";
+const dbUserName = "sanjaysaravanan1997";
+const dbPassword = "8JMtgqJdaf7R0WLm";
 
 const localUri = `mongodb://${dbCluster}/${dbName}`;
 
+const cloudUri = `mongodb+srv://${dbUserName}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+
 const mongooseConnect = async () => {
   try {
-    await mongoose.connect(localUri);
+    // await mongoose.connect(localUri);
+    await mongoose.connect(cloudUri);
     console.log("Mongoose Connection established");
   } catch (e) {
     console.log("Mongoose Connection error: " + e.message);
